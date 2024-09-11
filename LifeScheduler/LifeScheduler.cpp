57e159e4b9b95ba638a538207e033e971bd0ef72
Daniel Orozco <danybeam@gmail.com>
2024-09-11 17:40:26 -0400

    setup barebones project

---
+++ LifeScheduler/LifeScheduler.cpp
@@ -0,0 +1,13 @@
+﻿// LifeScheduler.cpp : Defines the entry point for the application.
+//
+
+#include <LifeScheduler/LifeScheduler.h>
+#include <libLifeSchedulerFramework/LifeSchedulerFramework.h>
+
+using namespace std;
+
+int main()
+{
+	doExternalThing();
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LifeScheduler
 A project that uses the libLifeSchedulerFramework library
+Following https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2018/p1204r0.html for project structure
