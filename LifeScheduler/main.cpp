b7a3345848911d19f770077bb80f5f949222a7d2
Daniel Orozco <danybeam@gmail.com>
2024-09-16 20:37:56 -0400

    Update lib submodule and implement informal test code

--- LifeScheduler/main.cpp
+++ LifeScheduler/main.cpp
@@ -2,34 +2,57 @@
 
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
+#include <libLifeSchedulerFramework/Item.h>
+#include <queue>
+
+using namespace LifeScheduler;
 
 int main()
 {
-    InitWindow(400, 200, "raygui - controls test suite");
-    SetTargetFPS(60);
+	InitWindow(400, 200, "raygui - controls test suite");
+	SetTargetFPS(60);
 
-    bool showMessageBox = false;
+	bool showMessageBox = false;
+	std::string name1 = "first";
+	std::string name2 = "second";
+	std::string name3 = "third";
+	Item item1(name1,1);
+	Item item2(name2,1);
+	Item item3(name3,2);
+	item2.AddWorkUnits(69);
 
-    while (!WindowShouldClose())
-    {
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+	std::priority_queue<Item> queue;
+	queue.push(item2);
+	queue.push(item1);
+	queue.push(item3);
 
-        if (GuiButton(Rectangle{ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+	std::string test("");
+	while (!queue.empty()) {
+		Item iter = queue.top();
+		queue.pop();
+		test = test + iter.GetName() + " priority: " + std::to_string(iter.GetPriority()) + " worked units: " + std::to_string(iter.GetWorkedUnits()) + "\n";
+	}
 
-        if (showMessageBox)
-        {
-            int result = GuiMessageBox(Rectangle{ 85, 70, 250, 100 },
-                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+	while (!WindowShouldClose())
+	{
+		// Draw
+		//----------------------------------------------------------------------------------
+		BeginDrawing();
+		ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-            if (result >= 0) showMessageBox = false;
-        }
+		if (GuiButton(Rectangle{ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
 
-        EndDrawing();
-    }
+		if (showMessageBox)
+		{
+			int result = GuiMessageBox(Rectangle{ 85, 70, 250, 100 },
+				"#191#Message Box", test.c_str(), "Nice;Cool");
 
-    CloseWindow();
-    return 0;
+			if (result >= 0) showMessageBox = false;
+		}
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
 }
