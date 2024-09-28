8d428aa8ef6e849e06f5da8f1149359f1f207804
Daniel Orozco <danybeam@gmail.com>
2024-09-27 20:05:24 -0400

    add function signature and comment for update components

--- LifeScheduler/IWindowElement.h
+++ LifeScheduler/IWindowElement.h
@@ -6,6 +6,10 @@ namespace LifeScheduler::View
 	{
 	public:
 		virtual ~IWindowElement() {};
+
+		// Use update to handle/call callbacks needs to be implemented per subclass since they do different things
+		virtual void Update() = 0; 
+		// Draw The component
 		virtual void Draw() = 0;
 	private:
 		// TODO(danybeam): Add callback function member and make mandatory somehow
